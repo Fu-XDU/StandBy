4acf41613ccf34f7323553995b2d5d95324c699d
Fu-XDU <fuming.137@icloud.com>
2024-07-12 20:52:44 +0800

    Initial Commit

---
+++ StandBy/StandByApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandByApp.swift
+//  StandBy
+//
+//  Created by 付铭 on 2024/7/12.
+//
+
+import SwiftUI
+
+@main
+struct StandByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
